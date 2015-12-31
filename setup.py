a169bd5a569f5ed26719925245ab165b7388ac2a
Matt Gormley <mrg@cs.jhu.edu>
2015-12-31 09:30:53 -0500

    Fixing encoding issue

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 setup(
     name='concrete_chunklink',
     version='0.1',
-    description='Wrapper of the chunklink.pl script used by CoNLL-2000 for use on Concrete Communications.'
+    description='Wrapper of the chunklink.pl script used by CoNLL-2000 for use on Concrete Communications.',
     author='Matt Gormley',
     author_email='mrg@cs.jhu.edu',
     url='http://www.cs.jhu.edu/~mrg/',
